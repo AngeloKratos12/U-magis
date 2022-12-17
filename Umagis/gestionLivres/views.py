c67ad363db6318e28da6e930d53bd03d231e1aaf
AngeloKratos12 <miharimananaangelo@yahoo.com>
2022-12-17 16:57:06 +0300

    BDADDDATA

--- Umagis/gestionLivres/views.py
+++ Umagis/gestionLivres/views.py
@@ -14,7 +14,7 @@ def biblio(request):
         logged_user_id = request.session['logged_user_id']
         logged_user = Users.objects.get(id=logged_user_id)
         listbook = []
-        for index in range(1,3):
+        for index in range(0,5):
             book = Books.objects.get(id=index)
             idBook = book.id
             cotation = book.cotation
@@ -132,7 +132,7 @@ def addBook(request):
             ##Add the book to the base
             numero, auteur, titre, edition, annee  = request.POST['numero'], request.POST['auteur'], request.POST['titre'],request.POST['edition'],request.POST['annee']
             lieu, cotation, etat, observation, categorie = request.POST['lieu'],request.POST['cotation'],request.POST['etat'],request.POST['observation'],request.POST['categorie']
-            addbook = models.Books(numero=numero, auteur=auteur, titre=titre, edition=edition, annee=annee,
+            addbook = Books(numero=numero, auteur=auteur, titre=titre, edition=edition, annee=annee,
                                    lieu=lieu, cotation=cotation, etat=etat, observation=observation, categorie=categorie)
             addbook.save()
 
